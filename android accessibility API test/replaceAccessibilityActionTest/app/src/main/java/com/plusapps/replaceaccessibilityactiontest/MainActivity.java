40165080b435470c7ba984e41fe8d4893f067500
Jeonggyu Park <goodsogi@gmail.com>
2020-10-28 11:14:35 +0900

    AccessibilityViewCommand의 리턴값에 따른 처리 확인

--- android accessibility API test/replaceAccessibilityActionTest/app/src/main/java/com/plusapps/replaceaccessibilityactiontest/MainActivity.java
+++ android accessibility API test/replaceAccessibilityActionTest/app/src/main/java/com/plusapps/replaceaccessibilityactiontest/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
 import androidx.core.view.accessibility.AccessibilityViewCommand;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,10 +31,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Log.d("plusapps", "버튼 클릭!");
+            }
+        });
+
 
         ViewCompat.replaceAccessibilityAction(button, AccessibilityNodeInfoCompat.AccessibilityActionCompat.ACTION_CLICK, "클릭", new AccessibilityViewCommand() {
             @Override
             public boolean perform(@NonNull View view, @Nullable CommandArguments args) {
+                Log.d("plusapps", "replaceAccessibilityAction 버튼 클릭!");
+                //return true를 하면 여기서 끝남. false를 하면 ClickListener가 호출됨
                 return true;
             }
         });
